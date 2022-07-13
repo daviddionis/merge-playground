ba34f76c948b4942e71b2c31ac2a9f405ee4accb
David Dionis Soler <daviddionissoler@gmail.com>
2022-07-13 21:59:40 +0200

    New function: multiply arrays

--- prueba.js
+++ prueba.js
@@ -1,5 +1,13 @@
 
 
+function multiplyArrays(arr1, arr2) {
+    var result = [];
+    for (var i = 0; i < arr1.length; i++) {
+        result.push(arr1[i] * arr2[i]);
+    }
+    return result;
+}
+
 function addArrays(arr1, arr2) {
     var result = [];
     for (var i = 0; i < arr1.length; i++) {
@@ -12,3 +20,5 @@ const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 
 console.log(addArrays(arr1, arr2));
+
+console.log(multiplyArrays(arr1, arr2));
